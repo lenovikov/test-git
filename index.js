bcf0fa4656654979b3e326e3d09b3447b51ed45e
Alexey Novikov <le.novikovv@gmail.com>
2024-07-21 18:24:31 +0300

    clear files

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log('main-0')
-console.log('test-m-0')
-console.log('test-m-1')
-console.log('est-m1-0')
--- test-branch.js
+++
@@ -1,6 +0,0 @@
-console.log('new branch')
-console.log('test branch')
-
-console.log('test branch 2')
-
-log('test branch - 3version')
